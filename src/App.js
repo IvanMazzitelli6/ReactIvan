1373f3d4ffc724193fb399bb42a4804b9e2e6744
MacarenaAbregu <103336385+MacarenaAbregu@users.noreply.github.com>
2022-08-06 10:54:03 -0300

    TP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Header"
+import Nav from "./Nav"
+import Footer from "./Footer"
+function App(){
+  return(
+<>
+<Header></Header>
+  <main>
+    <h2>Home</h2>
+  </main>
+ <Footer test={true} nombre="Tomas" ></Footer>
+</>
+  )
 }
 
-export default App;
+
+export default App
